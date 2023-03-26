e32f140650eb57af716300ddfa648eea1fa51c4c
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-03-26 14:00:36 -0500

    Started on the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import Dictionary from "./Dictionary"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <h1>DICTIONARY</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Claudia Vashchenko, open-sourced on Github.</footer>
+      </div>
     </div>
   );
 }
