15fd26ad4e853f106d0ec5611f7a25e8cfb544e5
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-03-27 21:06:25 -0500

    Added the images api and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data);
         setResults(response.data);
 
     }
+    function handleImagesResponse(response) {
+        setPhotos(response.data.photos);
+      }
 
     function search(){
         let apiKey = "6734baf8b2off87a45fcbec75c30t717";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+        axios.get(imagesApiUrl).then(handleImagesResponse);
     }
 
     function handleSubmit(event) {
@@ -47,6 +54,7 @@ export default function Dictionary(props) {
             <div className="hint">Suggested: beauty, art, culture, yoga, bohemian, music...</div>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     )
     } else {
